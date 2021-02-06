44b4dfa11748cabc3a0e4333c6ec8b3748d9c29b
Arafath1019 <arafath.yeasin1019@gmail.com>
2021-02-06 14:30:46 +0600

    Updating user model, auth middleware, and user post routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Auth Route');
+const User = require('../../models/User');
+
+const auth = require('../../middleware/auth');
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json('Server Error');
+    }
 });
 
 module.exports = router;
